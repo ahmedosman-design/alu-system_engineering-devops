66634fa1bb55dbc878d4277019f7b4c27db8cdf0
ahmedosman-design <a.osman@alustudent.com>
2026-08-01 00:07:43 +0300

    Fix Puppet manifest to target /etc/ssh/ssh_config

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,11 @@
 # Configures the local SSH client to use ~/.ssh/school and disable password authentication
-file { '/root/.ssh/config':
-  ensure => file,
-}
-
 file_line { 'Turn off passwd auth':
-  path    => '/root/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  require => File['/root/.ssh/config'],
 }
 
 file_line { 'Declare identity file':
-  path    => '/root/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   require => File_line['Turn off passwd auth'],
 }
